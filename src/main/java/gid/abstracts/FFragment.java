8f21b84a0da89a3cfc6b478fe7d74d89a37feef8
Gideon Bar <T_GideonB1@verifone.com>
2015-11-29 11:38:37 +0200

    responsive fragment

---
+++ src/main/java/gid/abstracts/FFragment.java
@@ -0,0 +1,11 @@
+package gid.abstracts;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by T_GideonB1 on 29/11/2015.
+ */
+public abstract class FFragment extends Fragment
+{
+    public abstract void refresh();
+}
